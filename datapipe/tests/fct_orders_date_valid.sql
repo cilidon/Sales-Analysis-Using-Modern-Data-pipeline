7f51a8267cf5e9b08d491d6c0c781ecd253b7cb4
Mandar Mhatre <63152566+cilidon@users.noreply.github.com>
2024-10-06 16:47:34 -0400

    Update fct_orders_date_valid.sql

--- datapipe/tests/fct_orders_date_valid.sql
+++ datapipe/tests/fct_orders_date_valid.sql
@@ -4,5 +4,5 @@ from
 {{ref('fct_orders')}}
 
 where
-date(order_date)>CURRENT_DATE()
-or date(order_date) < date('1990-01-01')
+date(order_date)<CURRENT_DATE()
+or date(order_date) > date('1990-01-01')
